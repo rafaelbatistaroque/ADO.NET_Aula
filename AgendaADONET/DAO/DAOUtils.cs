369a298addd156bee4b50b2b8fa9f8f7e020e722
Rafael Batista <maranata_rafa@hotmail.com>
2019-04-18 23:19:05 -0400

    Add Class of connection with DB

---
+++ AgendaADONET/DAO/DAOUtils.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaADONET.DAO
+{
+    class DAOUtils
+    {
+        public static DbConnection GetConexao()
+        {
+            string strConexao = @"Server=localhost;Database=db_dev;Uid=root;Pwd=9gregokirios;";
+            DbConnection conexao = new MySqlConnection(strConexao);
+            conexao.Open();
+            return conexao;
+        }
+        public static DbCommand GetComando(DbConnection conexao)
+        {
+            DbCommand comando = conexao.CreateCommand();
+            return comando;
+        }
+        public static DbDataReader GetDataReader(DbCommand comando)
+        {
+            return comando.ExecuteReader();
+        }
+    }
+}

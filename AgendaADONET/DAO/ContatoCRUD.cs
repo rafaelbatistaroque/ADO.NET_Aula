5fa9c676e4d52945626c5889056aebc1bdcc4d86
Rafael Batista <maranata_rafa@hotmail.com>
2019-04-20 20:08:03 -0400

    Nome alterado e método Excluir adicionado

---
+++ AgendaADONET/DAO/ContatoCRUD.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaADONET.DAO
+{
+    public class ContatoCRUD
+    {
+        public DataTable GetContatos()
+        {
+            DbConnection conexao = DAOUtils.GetConexao();
+            DbCommand comando = DAOUtils.GetComando(conexao);
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "SELECT * FROM tbl_aluno";
+            DbDataReader reader = DAOUtils.GetDataReader(comando);
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+
+            return dataTable;
+        }
+        public void Excluir(int id)
+        {
+            DbConnection conexao = DAOUtils.GetConexao();
+            DbCommand comando = DAOUtils.GetComando(conexao);
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "DELETE FROM tbl_aluno WHERE id_aluno = @id";
+            comando.Parameters.Add(new MySqlParameter("@id", id));
+
+            comando.ExecuteNonQuery();
+        }
+    }
+}
